575f19a2ae8e85912e8373539801f41905bf028e
narasimnsl <45248356+narasimnsl@users.noreply.github.com>
2018-11-23 06:45:36 +1100

    abbr fixes for th

--- src/components/pages/s01p01/abbrev.js
+++ src/components/pages/s01p01/abbrev.js
@@ -5,23 +5,25 @@ const abbrev = (props) => {
 
     let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
    
-
     let abb = ""
-    let str = props.getDate()
-    switch(str) {
-        case 1:
+    let str = ""+props.getDate();
+    let res = str.slice(-1);
+    switch(res) {
+        case "1":
         abb = "st"
             break;
-        case 2:
+        case "2":
         abb = "nd"
             break;
-        case 3:
+        case "3":
         abb = "rd"
             break;
         default:
         abb = "th"
     }
-
+    if(str === '11'||str === '12'||str === '13' ){
+        abb = "th"
+    }
     let rentDate = month[props.getMonth()]+', '+props.getDate()+abb+' '+props.getFullYear()
     
     return (
